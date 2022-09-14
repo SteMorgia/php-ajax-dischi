f63a19452bbe0dfeac3935ad93959288fe46a3df
Stefano Morgia <ste.morgia@gmail.com>
2022-09-14 15:38:10 +0200

    star: inizializzazione progetto

---
+++ primaVersione/index.php
@@ -0,0 +1,34 @@
+<?php
+
+include __DIR__ . '/../database.php';
+
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prima Versione</title>
+</head>
+<body>
+    
+    <?php
+
+        foreach($database as $cards => $singleCard) {
+            echo "<div> <img src=" . $singleCard["poster"] . "/>  </div>";
+        }
+
+
+    ?>
+
+
+
+</body>
+</html>
